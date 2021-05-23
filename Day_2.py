790b9c4f615b803522946fd0f2ef37ac7c99d7e4
Ritika <83857624+ritika21022001@users.noreply.github.com>
2021-05-23 12:48:24 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,13 @@
+#First_Program
+
+Age=20
+Str="My Age is:{}" 
+print(Str.format(Age))
+
+#Second_Program
+
+Age=25
+Name="Rahul"
+Str="My name is {} and my age is {}"
+print(Str.format(Name,Age))
+
